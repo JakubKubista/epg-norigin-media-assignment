8b4ae90cc166af445d0ef9febae9f1bc692bc32a
Jakub Kubišta <xkubista@gmail.com>
2019-06-13 22:27:01 +0200

    add unit test for props

---
+++ tests/unit/toolbar.spec.js
@@ -0,0 +1,17 @@
+import { mount  } from "@vue/test-utils";
+import Toolbar from "@/components/Toolbar.vue";
+
+describe("Toolbar.vue", () => {
+  const iconLeft = 'person';
+  const background = "#000000";
+
+  const wrapper = mount (Toolbar, {
+    propsData: { iconLeft, background }
+  });
+
+  it("renders props.msg when passed", () => {
+    expect(wrapper.isEmpty()).toBe(false);
+    expect(wrapper.find('.v-toolbar').exists()).toBe(true);
+    expect(wrapper.text()).toMatch(iconLeft);
+  });
+});
