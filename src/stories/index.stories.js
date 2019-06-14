d0c9b301ffbc27563df7ef132754732b54bb1ec6
kubistaja <kubista.jakub@allrisk.cz>
2019-06-14 10:17:15 +0200

    remove storybook init component, rename button

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,29 +3,10 @@ import { storiesOf } from "@storybook/vue";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
-import MyButton from "@/components/MyButton.vue";
+import ButtonSquare from "@/components/ButtonSquare.vue";
 import Toolbar from "@/components/Toolbar.vue";
 import TabsDate from "@/components/TabsDate.vue";
 import BottomNavigation from "@/components/BottomNavigation.vue";
-
-storiesOf("Button", module)
-  .add("with text", () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action("clicked") }
-  }))
-  .add("with JSX", () => ({
-    components: { MyButton },
-    render() {
-      return <my-button onClick={this.action}>With JSX</my-button>;
-    },
-    methods: { action: linkTo("Button", "with some emoji") }
-  }))
-  .add("with some emoji", () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action("clicked") }
-  }));
 
 storiesOf("Components", module)
   .add(
@@ -52,13 +33,20 @@ storiesOf("Components", module)
       notes: "Tabs for date selection. Today is default."
     }
   )
-    .add(
-      "BottomNavigation",
-      () => ({
-        components: { BottomNavigation },
-        template: '<BottomNavigation />'
-      }),
-      {
-        notes: "The main navigation of application."
-      }
-  );
+  .add(
+    "BottomNavigation",
+    () => ({
+      components: { BottomNavigation },
+      template: '<BottomNavigation />'
+    }),
+    {
+      notes: "The main navigation of application."
+    }
+  )
+  .add(
+    "ButtonSquare",
+    () => ({
+      components: { ButtonSquare },
+      template: '<ButtonSquare @click="action">Hello Button</ButtonSquare>',
+      methods: { action: action("clicked") }
+    }));
