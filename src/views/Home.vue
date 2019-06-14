96251badb00f8b1a1748bc896943491f9c11216e
kubistaja <kubista.jakub@allrisk.cz>
2019-06-14 11:47:15 +0200

    add button toggle into the view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
+    <ButtonToggle @click="favourite = $event"
+                  :icon="toggleIcon" />
     <TabsDate />
   </div>
 </template>
 
 <script>
+import ButtonToggle from "@/components/ButtonToggle";
 import TabsDate from "@/components/TabsDate";
 
 export default {
   name: "home",
   components: {
+    ButtonToggle,
     TabsDate
   },
   data() {
     return {
-      //
+      toggleIcon: "star",
+      favourite: false
     };
   }
 };
