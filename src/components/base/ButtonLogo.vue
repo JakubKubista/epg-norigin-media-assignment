e325264fe5c9aaa9a5dedb55293610a95728e2a1
Jakub Kubišta <xkubista@gmail.com>
2019-06-15 22:57:08 +0200

    add styles for button logo component

--- src/components/base/ButtonLogo.vue
+++ src/components/base/ButtonLogo.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-btn dark
-         fab
-         class="md-button-square">
-    <v-img :src="logo" :max-width="36" />
-  </v-btn>
+  <div>
+    <v-btn dark
+           fab
+           class="md-button-square md-button-logo">
+      <v-img :src="logo"
+             :max-width="36" />
+    </v-btn>
+  </div>
 </template>
 
 <script>
 export default {
   name: "button-logo",
   props: {
-    logo: String,
-  },
+    logo: String
+  }
 };
 </script>
