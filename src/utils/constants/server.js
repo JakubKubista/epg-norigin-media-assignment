a0099b06903e9654a0f1a28096fb04d9d8324d6b
Jakub Kubišta <xkubista@gmail.com>
2019-06-15 17:36:16 +0200

    add server urls and services

---
+++ src/utils/constants/server.js
@@ -0,0 +1,22 @@
+/**
+ * Constants for a server config.
+ * @module utils/isac/constants
+ * @see utils/config/server
+ * @see views
+ */
+export default {
+  url: {
+    local: 'http://localhost:1337/',
+    dev: 'http://localhost:1337/',
+    test: 'http://localhost:1337/',
+    release: 'http://localhost:1337/'
+  },
+  service: {
+    genres: 'genres',
+    slides: 'slides',
+    epg: 'epg',
+    favoriteChannels: 'favoriteChannels',
+    channel: 'channel',
+    program: 'program',
+  },
+};
