0eadb77f8452eb21db90d01550b8ab11ae99ed76
Jakub Kubišta <xkubista@gmail.com>
2019-06-15 22:01:18 +0200

    add labels for messages

---
+++ src/utils/constants/labels.js
@@ -0,0 +1,14 @@
+/**
+ * Constants for labels of whole app.
+ * @see components
+ * @see views
+ */
+
+export default {
+  message: {
+    fullScreen: {
+      notImplemented: "Comming soon",
+      notAvailable: "Not available",
+    }
+  }
+}
