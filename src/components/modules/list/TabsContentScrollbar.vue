bf06d894e99b66b93cea37aab36c213e516aa2cd
Jakub Kubišta <xkubista@gmail.com>
2019-06-16 21:12:18 +0200

    add method to set default scroll position

--- src/components/modules/list/TabsContentScrollbar.vue
+++ src/components/modules/list/TabsContentScrollbar.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import Dates from "@/utils/base/dates";
 import Timeline from "@/components/base/Timeline";
 
 export default {
@@ -14,16 +15,32 @@ export default {
   components: {
     Timeline
   },
+  data() {
+    return {
+      position: 0
+    };
+  },
   computed: {
     scrollbar: () => document.querySelector(".md-scrollbar")
   },
   methods: {
     handleScroll(event) {
-      document.querySelector(".md-content").scrollLeft = this.scrollbar.scrollLeft;
+      document.querySelector(
+        ".md-content"
+      ).scrollLeft = this.scrollbar.scrollLeft;
+    },
+    setDefaultScrollPosition() {
+      let scrollbarMax =
+        this.scrollbar.scrollWidth - this.scrollbar.clientWidth;
+      let timeProportion = Dates.getTodayTimeProportion();
+      this.scrollbar.scrollLeft = (scrollbarMax * timeProportion) / 100;
     }
   },
   mounted() {
     this.scrollbar.addEventListener("scroll", this.handleScroll);
+  },
+  updated() {
+    this.setDefaultScrollPosition();
   },
   beforeDestroy() {
     this.scrollbar.removeEventListener("scroll", this.handleScroll);
