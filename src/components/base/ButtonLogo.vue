78d2ecfa1cf11441c16c086cd16f4f366726d385
Jakub Kubišta <xkubista@gmail.com>
2019-06-16 13:59:40 +0200

    add tooltip for button logo

--- src/components/base/ButtonLogo.vue
+++ src/components/base/ButtonLogo.vue
@@ -1,17 +1,24 @@
 <template>
-    <v-btn dark
-           fab
-           class="md-button-square md-button-logo">
-      <v-img :src="logo"
-             :max-width="36" />
-    </v-btn>
+  <v-tooltip right>
+    <template v-slot:activator="{ on }">
+      <v-btn dark
+             fab
+             v-on="on"
+             class="md-button-square md-button-logo">
+        <v-img :src="logo"
+               :max-width="36" />
+      </v-btn>
+    </template>
+    <span> {{ title }} </span>
+  </v-tooltip>
 </template>
 
 <script>
 export default {
   name: "button-logo",
   props: {
-    logo: String
+    logo: String,
+    title: String
   }
 };
 </script>
