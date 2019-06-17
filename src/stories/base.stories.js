39b26cd274cf1ec0b7a0e8c6edd11fc25d2c85c8
Jakub Kubišta <xkubista@gmail.com>
2019-06-17 02:55:00 +0200

    update storybook by component changes

--- src/stories/base.stories.js
+++ src/stories/base.stories.js
@@ -5,11 +5,7 @@ import {
 import {
   action
 } from "@storybook/addon-actions";
-import {
-  linkTo
-} from "@storybook/addon-links";
 
-import CardSchedule from "@/components/base/CardSchedule.vue";
 import ButtonIcon from "@/components/base/ButtonIcon.vue";
 import ButtonLogo from "@/components/base/ButtonLogo.vue";
 import Timeline from "@/components/base/Timeline.vue";
@@ -17,25 +13,6 @@ import Timeline from "@/components/base/Timeline.vue";
 import "@/assets/styles/main.scss";
 
 storiesOf("Base", module)
-  .add(
-    "CardSchedule",
-    () => ({
-      components: {
-        CardSchedule
-      },
-      computed: {
-        title: () => "Vikings",
-        subtitle: () => "14:20 - 15:20",
-        active: () => true,
-        length: () => 60
-      },
-      template: '<CardSchedule @click="action" :title="title" :subtitle="subtitle" :active="active" :length="length" />',
-      methods: {
-        action: linkTo("Components", "ButtonIcon")
-      }
-    }), {
-      notes: "Single element for each show."
-    })
   .add(
     "ButtonIcon",
     () => ({
@@ -45,7 +22,7 @@ storiesOf("Base", module)
       computed: {
         icon: () => "star",
       },
-      template: '<ButtonIcon @click="action" :icon="icon" />',
+      template: '<ButtonIcon @click="action" :icon="icon" style="left: 0px;" />',
       methods: {
         action: action("clicked")
       }
@@ -60,8 +37,9 @@ storiesOf("Base", module)
       },
       computed: {
         logo: () => "https://img.noriginmedia.com/cloudberry/logo_sky1.png",
+        title: () => "Sky1",
       },
-      template: '<ButtonLogo :logo="logo" />',
+      template: '<ButtonLogo :logo="logo" :title="title" />',
       methods: {
         action: action("clicked")
       }
--- src/stories/modules.list.stories.js
+++ src/stories/modules.list.stories.js
@@ -1,36 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
+import {
+  linkTo
+} from "@storybook/addon-links";
 
-import Content from "@/components/modules/list/Content.vue";
-import Tab from "@/components/modules/list/Tab.vue";
 import Tabs from "@/components/modules/list/Tabs.vue";
+import TabsContent from "@/components/modules/list/TabsContent.vue";
+import TabsContentCard from "@/components/modules/list/TabsContentCard.vue";
+import TabsContentScrollbar from "@/components/modules/list/TabsContentScrollbar.vue";
+import TabsTab from "@/components/modules/list/TabsTab.vue";
 
 import "@/assets/styles/main.scss";
 
 storiesOf("Modules/List", module)
-  .add(
-    "Content",
-    () => ({
-      components: { Content },
-      template: '<Content />'
-    }),
-    {
-      notes: "Content for a whole schedule in each tab."
-    }
-  )
-  .add(
-    "Tab",
-    () => ({
-      components: { Tab },
-      computed: {
-        order: () => '1'
-      },
-      template: '<Tab :order="order" />'
-    }),
-    {
-      notes: "Tab is simple component for each field of tabs."
-    }
-  )
   .add(
     "Tabs",
     () => ({
@@ -40,4 +22,55 @@ storiesOf("Modules/List", module)
     {
       notes: "Tabs for date selection. Today is default."
     }
-  );
+  )
+  .add(
+    "TabsContent",
+    () => ({
+      components: { TabsContent },
+      template: '<TabsContent />'
+    }),
+    {
+      notes: "Content for a whole schedule in each tab."
+    }
+  )
+  .add(
+    "TabsContentCard",
+    () => ({
+      components: {
+        TabsContentCard
+      },
+      computed: {
+        title: () => "Vikings",
+        start: () => "2018-10-26T23:58:00+02:00",
+        end: () => "2018-10-26T23:59:00+02:00"
+      },
+      template: '<TabsContentCard @click="action" :title="title" :start="start" :end="end" style="width:200px;" />',
+      methods: {
+        action: linkTo("Components", "TabsTab")
+      }
+    }), {
+      notes: "Single element for each show."
+    })
+    .add(
+      "TabsContentScrollbar",
+      () => ({
+        components: { TabsContentScrollbar },
+        template: '<TabsContentScrollbar />'
+      }),
+      {
+        notes: "Special scrollbar for above position."
+      }
+    )
+    .add(
+      "TabsTab",
+      () => ({
+        components: { TabsTab },
+        computed: {
+          order: () => '1'
+        },
+        template: '<TabsTab :order="order" />'
+      }),
+      {
+        notes: "Tab is simple component for each field of tabs."
+      }
+    );
